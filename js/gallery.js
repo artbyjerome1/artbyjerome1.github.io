31930270aebef44cc436dbaecd569c936035fccc
HeidiPo <kittenviolin@gmail.com>
2019-11-24 19:35:57 -0500

    Update gallery.js
    created script for adding all images in sea gallery to page

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,6 @@
 
+let n = 3; // n
+let gallery = document.getElementById('sea_gallery');
+for(let i = 1; i <= n ; i++) {
+   sea_gallery.innerHTML += '<img src="yourfolder/'+ i +'.jpg" + 'width="30%"'/>';
+}
